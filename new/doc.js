76f4eb22d74d4bb8f17585a179a0283edce70b53
adonaygebrerufael <140199006+adonaygebrerufael@users.noreply.github.com>
2023-11-30 22:48:35 -0600

    m

---
+++ new/doc.js
@@ -0,0 +1,26 @@
+/*function foo(){
+    console.log("no agrument");
+}
+function foo(arg1){
+    console.log(arg1);
+}
+function foo(arg1,arg2){
+    console.log(arg1,arg2);
+}
+function foo(arg1,arg2,arg3){
+    console.log(arg1,arg2);
+}
+foo()
+foo(1)
+foo(1,2)
+foo(1,2)*/
+ const str="Hello";
+ const arr=[...str];
+ console.log(arr);
+
+ 
+
+
+
+
+
