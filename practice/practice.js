cff751ff2ce9b0fe94caa8fce64a5fe799aebe95
adonaygebrerufael <140199006+adonaygebrerufael@users.noreply.github.com>
2023-12-12 23:55:28 -0600

    changes

---
+++ practice/practice.js
@@ -0,0 +1,74 @@
+// let libraryBooks = [
+//     { title: "The Road Ahead", author: "Bill Gates", libraryID: 1235 },
+//     { title: "Walter Isaacson", author: "Steve Jobs", libraryID: 4268 },
+//     { title: "The Road Ahead", author: "Bill Gates", libraryID: 4268 },
+//     { title: "Mockingjay: The Final Book of The Hunger Games", author: "Suzanne Collins", libraryID: 3257 }
+//     ];
+//    function addbook(title,author,libraryId){
+//      let  book={
+//            title,
+//            author,
+//            libraryId,
+//    }
+//        libraryBooks.push(book);
+       
+// }
+
+
+// function getTitles(libraryBooks){
+//     let titles=[];
+//     for(let i=0;i<libraryBooks.length;i++){
+//         titles.push(libraryBooks[i].title);
+//     }
+//     titles.sort();
+//     return titles;
+// }
+
+//  function findbook(e){
+
+//     for (let index = 0; index < libraryBooks; index++) {
+//         if(libraryBooks[i].title.includes("e")){
+//             libraryBooks.books;
+
+//         }
+//         books.sort();
+//         return books
+      
+        
+//     }
+//  }
+// //  findBooks, which will take the keyword of title as input. It will find books which contain the given keyword in
+// //  the title and return books alphabetically ordered by the author use for loop
+
+// functionfindBooks(keyword){     
+//     for(let i=0;i<libraryBooks.length;i++){
+//         if(libraryBooks[i].title.includes(keyword)){
+//             libraryBooks[i].author
+//         }
+//     }
+//     return books;
+// }
+    
+       
+   
+   
+//    addbook("tomas","nahom",123);
+
+//    console.log(getTitles(libraryBooks));
+
+
+
+//     console.log(libraryBooks);
+
+
+
+let div=document.createElement("div")
+let p=document.createElement("p")
+let text =document.createTextNode("it is p tag")
+p.appendChild(text)
+div.appendChild(p)
+document.body.appendChild(div)
+// document.removeChild(element)
+// document.appenendchild(element)
+// document.replaceChild(new'old)
+// document.write(text)
