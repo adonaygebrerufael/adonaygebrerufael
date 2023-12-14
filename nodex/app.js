da918c3bf47d01824aaf52b122e8df354512ec38
adonaygebrerufael <140199006+adonaygebrerufael@users.noreply.github.com>
2023-12-13 21:39:14 -0600

    chages

--- nodex/app.js
+++ nodex/app.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+// var a=7;
+// var b=2;
+// var c=a+b;
+
+
+
+
+
+// console.log(c);
+var http=require('http');
+http.createServer(function(reg,res){
+
+
+// createServer(function(reg,res){
+
+    res.write("welcome back alien !!!")
+    res.end()
+}).listen(8080)
+
