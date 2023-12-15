c031371b9b97b58130444d0c2411ac5978abc7fc
adonaygebrerufael <140199006+adonaygebrerufael@users.noreply.github.com>
2023-12-15 10:57:34 -0600

    coomit

--- nodex/app.js
+++ nodex/app.js
@@ -6,14 +6,18 @@
 
 
 
-// console.log(c);
-var http=require('http');
-http.createServer(function(reg,res){
+// Extending the Array prototype
+Array.prototype.even = function() {
+    return this.filter(num => num % 2 === 0);
+};
 
+Array.prototype.odd = function() {
+    return this.filter(num => num % 2 !== 0);
+};
 
-// createServer(function(reg,res){
+// Testing the functionality
+const myArray = [1,2,3,4,5,6,7,8];
+console.log("Even Numbers: ", myArray.even()); // [2,4,6,8]
+console.log("Odd Numbers: ", myArray.odd()); // [1,3,5,7]
 
-    res.write("welcome back alien !!!")
-    res.end()
-}).listen(8080)
 
--- nodex/exa.js
+++ nodex/exa.js
@@ -1,1 +1,9 @@
-de
+// create http server
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.end('Welcome to our home page 555.');
+});
+server.listen(3000,
+    ()=>{
+        console.log("server is started");
+    });
